ab44ecb6b514671cd62f3ebe2fc4a56cc24fab6f
Jesse <jesserosshutt@gmail.com>
2021-02-16 12:14:11 +1300

    add and setup stripe button component

--- src/components/stripe-button/stripe-button.component.jsx
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -2,5 +2,28 @@ import React from 'react'
 import StripeCheckout from 'react-stripe-checkout'
 
 const StripeCheckoutButton = ({ price }) => {
-  
+  const priceForStripe = price * 100
+  const publishableKey = 'pk_test_51ILFN3CaOwz3k7RSbS4c2boLUpRIcDmj6VsftDycXpfdaKUE1pSXZbxuCSrM46YZ9BhY7z6br1UcTHjUfdztWage00N5y97Yga'
+
+  const onToken = token => {
+    console.log(token)
+    alert('Payment Successful')
+  }
+
+  return (
+    <StripeCheckout 
+      label='Pay Now'
+      name='CRWN Clothing Ltd.'
+      billingAddress
+      shippingAddress
+      image='https://svgshare.com/i/CUz.svg'
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel='Pay Now'
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  )
 }
+
+export default StripeCheckoutButton
--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -3,6 +3,7 @@ import { connect } from 'react-redux'
 import { createStructuredSelector } from 'reselect'
 
 import CheckoutItem from '../../components/checkout-item/checkout-item.component'
+import StripeCheckoutButton from ''
 
 import { selectCartItems, selectCartTotal } from '../../redux/cart/cart.selectors'
 
