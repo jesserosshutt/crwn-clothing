a1134892357843ed8976ae5fa2beeeaaa19f1520
Jesse <jesserosshutt@gmail.com>
2021-02-23 11:47:50 +1300

    migrate shop data to firebase

--- src/App.js
+++ src/App.js
@@ -12,8 +12,8 @@ import SignInAndSignUpPage from './pages/sign-in-and-sign-up-page/sign-in-and-si
 import CheckoutPage from './pages/checkout/checkout.component'
 
 import { auth, createUserProfileDocument } from './firebase/firebase.utils'
-import { setCurrentUser } from './redux/user/user.actions'
 
+import { setCurrentUser } from './redux/user/user.actions'
 import { selectCurrentUser } from './redux/user/user.selectors'
 
 class App extends React.Component {
@@ -22,7 +22,7 @@ class App extends React.Component {
 
   componentDidMount() {
 
-    const {setCurrentUser} = this.props
+    const { setCurrentUser } = this.props
 
     this.unsubscribeFromAuth = auth.onAuthStateChanged( async userAuth => {
       if (userAuth) {
@@ -32,10 +32,11 @@ class App extends React.Component {
           setCurrentUser({
               id: snapShot.id,
               ...snapShot.data()
-            })
+          })
         })
       } else {
         setCurrentUser(userAuth)
+        addCollectionAndDocuments('collections', collectionsArray.map(({title, items}) => ({title,items})))
       }
     })
   }
