dce48ec5732f68a55b1bc01fd6240938cd96d237
Jesse <jesserosshutt@gmail.com>
2021-02-23 14:12:12 +1300

    commit changes add unfinshed and broke

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,17 +1,22 @@
 import React from 'react'
 import { Route } from 'react-router-dom'
+import { connect } from 'react-redux'
 
 import CollectionsOverview from '../../components/collections-overview/collections-overview.component'
 import CollectionPage from '../collection/collection.component'
 
 import { firestore, convertCollectionsSnapshotToMap } from '../../firebase/firebase.utils'
 
+import { updateCollections } from '../../redux/shop/shop.actions'
+
 import './shop.styles.scss'
+import { updateCollections } from '../../redux/shop/shop.actions'
 
 class ShopPage extends React.Component {
   unsubscribeFromSnapshot = null
 
   componentDidMount() {
+    const { updateCollections } = this.props
     const collectionRef = firestore.collection('collections')
     
     // this.unsubscribeFromSnapshot = collectionRef.onSnapshot(async snapshot => {
@@ -19,7 +24,8 @@ class ShopPage extends React.Component {
     //   console.log(collectionsMap)
     // })
     collectionRef.onSnapshot(async snapshot => {
-      convertCollectionsSnapshotToMap(snapshot)
+      const collectionsMap = convertCollectionsSnapshotToMap(snapshot)
+      updateCollections(collectionsMap)
     })
   }
 
@@ -32,6 +38,10 @@ class ShopPage extends React.Component {
       </div>
     )
   }
-}  
+}
 
-export default ShopPage
+const mapDispatchToProps = dispatch => ({
+  updateCollections: collectionsMap => dispatch(updateCollections(collections))
+})
+
+export default connect(null, mapDispatchToProps)(ShopPage)
