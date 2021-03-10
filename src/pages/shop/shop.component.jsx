efa2962374a311e4a20b179ccfc6679babbdb8d1
Jesse <jesserosshutt@gmail.com>
2021-03-10 14:59:48 +1300

    start changing redux thunk into redux saga

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -5,15 +5,15 @@ import { connect } from 'react-redux'
 import CollectionsOverviewContainer from '../../components/collections-overview/collections-overview.container'
 import CollectionPageContainer from '../collection/collection.container'
 
-import { fetchCollectionsStartAsync } from '../../redux/shop/shop.actions'
+import { fetchCollectionsStart } from '../../redux/shop/shop.actions'
 
 import './shop.styles.scss'
 
 class ShopPage extends React.Component {
   componentDidMount() {
-    const { fetchCollectionsStartAsync } = this.props
+    const { fetchCollectionsStart } = this.props
 
-    fetchCollectionsStartAsync()
+    fetchCollectionsStart()
   }
 
   render() {
@@ -36,7 +36,7 @@ class ShopPage extends React.Component {
 }
 
 const mapDispatchToProps = dispatch => ({
- fetchCollectionsStartAsync: () => dispatch(fetchCollectionsStartAsync())
+ fetchCollectionsStart: () => dispatch(fetchCollectionsStart())
 })
 
 export default connect(null, mapDispatchToProps)(ShopPage)
